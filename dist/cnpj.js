7fd465c4548a6140b5f29f1619bed33b110ffb25
Nando Vieira <fnando.vieira@gmail.com>
2018-10-26 11:19:34 -0700

    Bump up version.

--- dist/cnpj.js
+++ dist/cnpj.js
@@ -96,6 +96,15 @@ var BLACKLIST = ["00000000000000", "11111111111111", "22222222222222", "33333333
 var STRICT_STRIP_REGEX = /[-\/.]/g;
 var LOOSE_STRIP_REGEX = /[^\d]/g;
 
+/**
+ * Compute the Verifier Digit (or "Dígito Verificador (DV)" in portuguese) for CNPJ.
+ *
+ * You can learn more about this on [wikipedia (pt-br)](https://pt.wikipedia.org/wiki/D%C3%ADgito_verificador)
+ *
+ * @export
+ * @param {string} numbers the CNPJ string with only numbers.
+ * @returns {number} the verifier digit.
+ */
 function verifierDigit(numbers) {
   var index = 2;
   var reverse = numbers.split("").reduce(function (buffer, number) {
@@ -112,17 +121,53 @@ function verifierDigit(numbers) {
   return mod < 2 ? 0 : 11 - mod;
 }
 
-function format(number) {
-  return strip(number).replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
+/**
+ * Transform the input into a pretty CNPJ format.
+ *
+ * Example:
+ * ```
+ * format('54550752000155');
+ * // Result: '54.550.752/0001-55'
+ * ```
+ *
+ * @export
+ * @param {string} cnpj the CNPJ.
+ * @returns {string} the formatted CNPJ.
+ */
+function format(cnpj) {
+  return strip(cnpj).replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
 }
 
-function strip(number, strict) {
-  var regex = strict ? STRICT_STRIP_REGEX : LOOSE_STRIP_REGEX;
-  return (number || "").toString().replace(regex, "");
+/**
+ * Remove some characters from the input.
+ *
+ * Example:
+ * ```
+ * strip('54550[752#0001..$55'); // Result: '54550752000155'
+ * strip('54550[752#0001..$55', true); // Result: '54550[752#0001..$55' - Atention!
+ * ```
+ *
+ * @export
+ * @param {string} cnpj the CNPJ text.
+ * @param {boolean} [isStrict] if `true`, it will remove only `.` and `-` characters.
+ *                             Otherwise, it will remove all non-digit (`[^\d]`) characters. Optional.
+ * @returns {string} the stripped CNPJ.
+ */
+function strip(cnpj, isStrict) {
+  var regex = isStrict ? STRICT_STRIP_REGEX : LOOSE_STRIP_REGEX;
+  return (cnpj || "").toString().replace(regex, "");
 }
 
-function isValid(number, strict) {
-  var stripped = strip(number, strict);
+/**
+ * Validate the CNPJ.
+ *
+ * @export
+ * @param {string} cnpj the CNPJ number.
+ * @param {boolean} [isStrict] if `true`, it will accept only `digits`, `.` and `-` characters. Optional.
+ * @returns {boolean} `true` if CNPJ is valid. Otherwise, `false`.
+ */
+function isValid(cnpj, isStrict) {
+  var stripped = strip(cnpj, isStrict);
 
   // CNPJ must be defined
   if (!stripped) {
@@ -146,7 +191,14 @@ function isValid(number, strict) {
   return numbers.substr(-2) === stripped.substr(-2);
 }
 
-function generate(formatted) {
+/**
+ * Generate a random CNPJ.
+ *
+ * @export
+ * @param {boolean} [useFormat] if `true`, it will format using `.` and `-`. Optional.
+ * @returns {string} the CNPJ.
+ */
+function generate(useFormat) {
   var numbers = "";
 
   for (var i = 0; i < 12; i += 1) {
@@ -156,7 +208,7 @@ function generate(formatted) {
   numbers += verifierDigit(numbers);
   numbers += verifierDigit(numbers);
 
-  return formatted ? format(numbers) : numbers;
+  return useFormat ? format(numbers) : numbers;
 }
 
 /***/ })
